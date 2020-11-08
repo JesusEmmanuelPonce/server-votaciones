9f0b65a0676e14d40a7334cb784e5cd6868c1c4a
Emmanuel Ponce <jesus.ponce.supernova@gmail.com>
2020-11-07 22:55:01 -0600

    sockets config

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express')
-const app = express();
 const path = require('path')
+require('dotenv').config()
 
+// Express
+const app = express()
+
+// Socket io
+const http = require('http').createServer(app)
+module.exports.io = require('socket.io')(http)
+require('./sockets/socket')
+
+// Path public
 const publicPath = path.resolve( __dirname, 'public' )
-
 app.use( express.static(publicPath) )
 
-app.listen(3000, () => {
-    console.log('Server ready')
+http.listen(process.env.PORT, () => {
+    console.log(`Server ${process.env.PORT}`)
 })
